72b876305343e53d55960aaca4cf1642bb113236
Prabhas Kurapati <prabhask@berkeley.edu>
2025-05-07 17:23:13 -0400

    added market data update struct for fastdds communication

---
+++ matching-engine/src/MarketDataUpdate.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <MarketDataIncrementalRefresh.hpp>
+
+// Represents a market data incremental update to send to the data service and
+// expose to the user via the FIX gateway.
+struct MarketDataUpdate {
+  // Symbol of stock ticker of the order book this update is coming from.
+  std::string symbol;
+
+  // Data sent.
+  DistributedStockExchange_MarketDataIncrementalRefresh::
+      MarketDataIncrementalRefresh refresh_data;
+};
+
+using MarketDataUpdatePtr = std::shared_ptr<MarketDataUpdate>;
