ea911c4a9b7a717af91018150024962de76d42fd
Prabhas Kurapati <prabhask@berkeley.edu>
2025-05-16 20:35:28 -0400

    small changes

--- library/LoggerHelper.hpp
+++ library/LoggerHelper.hpp
@@ -3,18 +3,18 @@
 #include <log4cxx/basicconfigurator.h>
 #include <log4cxx/logger.h>
 
-template <class STREAM, class DATS_LOGGER, class DATS_CLASS>
-inline void log_debug(const LOG4CXX_NS::LoggerPtr &logger, DATS_CLASS &obj,
+template <class STREAM, class DSE_LOGGER, class DSE_CLASS>
+inline void log_debug(const LOG4CXX_NS::LoggerPtr &logger, DSE_CLASS &obj,
                       const std::string &context) {
   STREAM ss;
-  DATS_LOGGER::log(ss, obj);
+  DSE_LOGGER::log(ss, obj);
   LOG4CXX_DEBUG(logger, context << ":" << ss.str());
 };
 
-template <class STREAM, class DATS_LOGGER, class DATS_CLASS>
-inline void log_info(const LOG4CXX_NS::LoggerPtr &logger, DATS_CLASS &obj,
+template <class STREAM, class DSE_LOGGER, class DSE_CLASS>
+inline void log_info(const LOG4CXX_NS::LoggerPtr &logger, DSE_CLASS &obj,
                      const std::string &context) {
   STREAM ss;
-  DATS_LOGGER::log(ss, obj);
+  DSE_LOGGER::log(ss, obj);
   LOG4CXX_INFO(logger, context << ":" << ss.str());
 };
