4d7e39601190d504cdf3a7f2693a415063e1cc66
Prabhas Kurapati <prabhask@berkeley.edu>
2025-05-18 16:37:42 -0400

    in fix gateway main.cpp, add more stack allocation

--- fix-gateway/src/main.cpp
+++ fix-gateway/src/main.cpp
@@ -33,8 +33,6 @@
 #include <SecurityListRequestPubSubTypes.hpp>
 #include <boost/asio.hpp>
 #include <boost/program_options.hpp>
-#include <boost/property_tree/ini_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
 #include <iostream>
 #include <quickfix/FileStore.h>
 #include <quickfix/SessionFactory.h>
@@ -68,9 +66,6 @@ int main(int argc, char *argv[]) {
     if (config_file.empty())
       throw std::runtime_error("Error: Config file name is not specified.");
 
-    boost::property_tree::ptree pt;
-    boost::property_tree::ini_parser::read_ini(config_file, pt);
-
     // Parse settings/preferences from config file.
     FixSettingsPtr settings_ptr =
         std::make_unique<FIX::SessionSettings>(config_file);
@@ -85,15 +80,15 @@ int main(int argc, char *argv[]) {
     // Parse env variables.
     auto home_env = std::getenv("DSE_HOME");
     auto log_env = std::getenv("EXCHANGE_LOGDIR");
-    if (home_env == NULL || log_env == NULL)
-      throw std::runtime_error("DSE_HOME or/and EXCHANGE_LOGDIR is not set");
+    if (home_env == nullptr || log_env == nullptr)
+      throw std::runtime_error("DSE_HOME and/or EXCHANGE_LOGDIR is not set");
 
     // Create quickfix data dictionary.
     FixDictionaryPtr default_dictionary_ptr = std::make_unique<FIX::Dictionary>(
         settings_ptr->get(default_session_id));
     default_dictionary_ptr->setString(
         "DATADICTIONARY",
-        std::string(home_env) + "/spec/" +
+        std::string(home_env) + "fix-gateway/spec/" +
             default_dictionary_ptr->getString("DATADICTIONARY"));
 
     // Parse data service name from data dictionary.
@@ -104,13 +99,12 @@ int main(int argc, char *argv[]) {
                                           << " |Data Service Name| "
                                           << data_service_name);
 
-    // Make DDS participant representing matching engine, and set up
+    // Make DDS participant representing FIX gateway, and set up
     // publisher/subscriber.
-    DefaultDomainParticipantPtr participant_ptr =
-        std::make_unique<DefaultDomainParticipant>(0, fix_gateway_name);
+    DefaultDomainParticipant participant(0, fix_gateway_name);
 
-    participant_ptr->create_publisher();
-    participant_ptr->create_subscriber();
+    participant.create_publisher();
+    participant.create_subscriber();
 
     // Make the FIX gateway data writer container dependency.
     DataWriterContainerPtr data_writer_container_ptr =
@@ -120,70 +114,65 @@ int main(int argc, char *argv[]) {
 
     // Logon request.
     auto logon_topic_tuple =
-        participant_ptr
-            ->make_topic<DistributedStockExchange_Logon::LogonPubSubType,
-                         DistributedStockExchange_Logon::Logon>(
-                LOGON_TOPIC_NAME);
+        participant.make_topic<DistributedStockExchange_Logon::LogonPubSubType,
+                               DistributedStockExchange_Logon::Logon>(
+            LOGON_TOPIC_NAME);
     data_writer_container_ptr->logOnDW =
-        participant_ptr->make_data_writer(logon_topic_tuple);
+        participant.make_data_writer(logon_topic_tuple);
 
     // New Order Single request.
-    auto new_order_single_topic_tuple = participant_ptr->make_topic<
+    auto new_order_single_topic_tuple = participant.make_topic<
         DistributedStockExchange_NewOrderSingle::NewOrderSinglePubSubType,
         DistributedStockExchange_NewOrderSingle::NewOrderSingle>(
         NEW_ORDER_SINGLE_TOPIC_NAME);
     data_writer_container_ptr->newOrderSingleDW =
-        participant_ptr->make_data_writer(new_order_single_topic_tuple);
+        participant.make_data_writer(new_order_single_topic_tuple);
 
     // Order Cancel request.
-    auto order_cancel_request_topic_tuple = participant_ptr->make_topic<
+    auto order_cancel_request_topic_tuple = participant.make_topic<
         DistributedStockExchange_OrderCancelRequest::
             OrderCancelRequestPubSubType,
         DistributedStockExchange_OrderCancelRequest::OrderCancelRequest>(
         ORDER_CANCEL_REQUEST_TOPIC_NAME);
     data_writer_container_ptr->orderCancelRequestDW =
-        participant_ptr->make_data_writer(order_cancel_request_topic_tuple);
+        participant.make_data_writer(order_cancel_request_topic_tuple);
 
     // Order Mass Cancel request.
     auto order_mass_cancel_request_topic_tuple =
-        participant_ptr
-            ->make_topic<DistributedStockExchange_OrderMassCancelRequest::
-                             OrderMassCancelRequestPubSubType,
-                         DistributedStockExchange_OrderMassCancelRequest::
-                             OrderMassCancelRequest>(
-                ORDER_MASS_CANCEL_REQUEST_TOPIC_NAME);
+        participant.make_topic<DistributedStockExchange_OrderMassCancelRequest::
+                                   OrderMassCancelRequestPubSubType,
+                               DistributedStockExchange_OrderMassCancelRequest::
+                                   OrderMassCancelRequest>(
+            ORDER_MASS_CANCEL_REQUEST_TOPIC_NAME);
     data_writer_container_ptr->orderMassCancelRequestDW =
-        participant_ptr->make_data_writer(
-            order_mass_cancel_request_topic_tuple);
+        participant.make_data_writer(order_mass_cancel_request_topic_tuple);
 
     // Order Mass Status request.
     auto order_mass_status_request_topic_tuple =
-        participant_ptr
-            ->make_topic<DistributedStockExchange_OrderMassStatusRequest::
-                             OrderMassStatusRequestPubSubType,
-                         DistributedStockExchange_OrderMassStatusRequest::
-                             OrderMassStatusRequest>(
-                ORDER_MASS_STATUS_REQUEST_TOPIC_NAME);
+        participant.make_topic<DistributedStockExchange_OrderMassStatusRequest::
+                                   OrderMassStatusRequestPubSubType,
+                               DistributedStockExchange_OrderMassStatusRequest::
+                                   OrderMassStatusRequest>(
+            ORDER_MASS_STATUS_REQUEST_TOPIC_NAME);
     data_writer_container_ptr->orderMassCancelRequestDW =
-        participant_ptr->make_data_writer(
-            order_mass_status_request_topic_tuple);
+        participant.make_data_writer(order_mass_status_request_topic_tuple);
 
     // Security List request.
-    auto security_list_request_topic_tuple = participant_ptr->make_topic<
+    auto security_list_request_topic_tuple = participant.make_topic<
         DistributedStockExchange_SecurityListRequest::
             SecurityListRequestPubSubType,
         DistributedStockExchange_SecurityListRequest::SecurityListRequest>(
         SECURITY_LIST_REQUEST_TOPIC_NAME);
     data_writer_container_ptr->securityListRequestDW =
-        participant_ptr->make_data_writer(security_list_request_topic_tuple);
+        participant.make_data_writer(security_list_request_topic_tuple);
 
     // Market Data request.
-    auto market_data_request_topic_tuple = participant_ptr->make_topic<
+    auto market_data_request_topic_tuple = participant.make_topic<
         DistributedStockExchange_MarketDataRequest::MarketDataRequestPubSubType,
         DistributedStockExchange_MarketDataRequest::MarketDataRequest>(
         MARKET_DATA_REQUEST_TOPIC_NAME);
     data_writer_container_ptr->marketDataRequestDW =
-        participant_ptr->make_data_writer(market_data_request_topic_tuple);
+        participant.make_data_writer(market_data_request_topic_tuple);
 
     // Make the FixLogFileFactory, FileStoreFactory dependencies.
     std::string fix_prefix =
@@ -201,17 +190,17 @@ int main(int argc, char *argv[]) {
     std::string auth_comp_id_filter = "SourceUser=%0";
 
     // Logon response.
-    auto logon_data_reader_tuple = participant_ptr->make_data_reader_tuple(
+    auto logon_data_reader_tuple = participant.make_data_reader_tuple(
         logon_topic_tuple, new LogonDataReaderListener(app), "FILTERED_LOGON",
         auth_comp_id_filter, {"AUTH"});
 
     // Logout request.
     auto logout_topic_tuple =
-        participant_ptr
-            ->make_topic<DistributedStockExchange_Logout::LogoutPubSubType,
-                         DistributedStockExchange_Logout::Logout>(
+        participant
+            .make_topic<DistributedStockExchange_Logout::LogoutPubSubType,
+                        DistributedStockExchange_Logout::Logout>(
                 LOGOUT_TOPIC_NAME);
-    auto logout_data_reader_tuple = participant_ptr->make_data_reader_tuple(
+    auto logout_data_reader_tuple = participant.make_data_reader_tuple(
         logout_topic_tuple, new LogoutDataReaderListener(app),
         "FILTERED_LOGOUT", auth_comp_id_filter, {"AUTH"});
 
@@ -219,72 +208,71 @@ int main(int argc, char *argv[]) {
     std::string target_comp_id_filter = "Destination=%0";
 
     // Security List response.
-    auto security_list_topic_tuple = participant_ptr->make_topic<
+    auto security_list_topic_tuple = participant.make_topic<
         DistributedStockExchange_SecurityList::SecurityListPubSubType,
         DistributedStockExchange_SecurityList::SecurityList>(
         SECURITY_LIST_TOPIC_NAME);
-    auto security_list_data_reader_tuple =
-        participant_ptr->make_data_reader_tuple(
-            security_list_topic_tuple, new SecurityListDataReaderListener(app),
-            "FILTERED_REF_DATA", target_comp_id_filter, {fix_gateway_name});
+    auto security_list_data_reader_tuple = participant.make_data_reader_tuple(
+        security_list_topic_tuple, new SecurityListDataReaderListener(app),
+        "FILTERED_REF_DATA", target_comp_id_filter, {fix_gateway_name});
 
     // Market Data Incremental Refresh response.
     auto market_data_incremental_refresh_topic_tuple =
-        participant_ptr
-            ->make_topic<DistributedStockExchange_MarketDataIncrementalRefresh::
-                             MarketDataIncrementalRefreshPubSubType,
-                         DistributedStockExchange_MarketDataIncrementalRefresh::
-                             MarketDataIncrementalRefresh>(
+        participant
+            .make_topic<DistributedStockExchange_MarketDataIncrementalRefresh::
+                            MarketDataIncrementalRefreshPubSubType,
+                        DistributedStockExchange_MarketDataIncrementalRefresh::
+                            MarketDataIncrementalRefresh>(
                 MARKET_DATA_INCREMENTAL_REFRESH_TOPIC_NAME);
     auto market_data_incremental_refresh_data_reader_tuple =
-        participant_ptr->make_data_reader_tuple(
+        participant.make_data_reader_tuple(
             market_data_incremental_refresh_topic_tuple,
             new MarketDataIncrementalRefreshDataReaderListener(app));
 
     // Market Data Snapshot Full Refresh response.
     auto market_data_snapshot_full_refresh_topic_tuple =
-        participant_ptr->make_topic<
-            DistributedStockExchange_MarketDataSnapshotFullRefresh::
-                MarketDataSnapshotFullRefreshPubSubType,
-            DistributedStockExchange_MarketDataSnapshotFullRefresh::
-                MarketDataSnapshotFullRefresh>(
-            MARKET_DATA_SNAPSHOT_FULL_REFRESH_TOPIC_NAME);
+        participant
+            .make_topic<DistributedStockExchange_MarketDataSnapshotFullRefresh::
+                            MarketDataSnapshotFullRefreshPubSubType,
+                        DistributedStockExchange_MarketDataSnapshotFullRefresh::
+                            MarketDataSnapshotFullRefresh>(
+                MARKET_DATA_SNAPSHOT_FULL_REFRESH_TOPIC_NAME);
     auto market_data_snapshot_full_refresh_data_reader_tuple =
-        participant_ptr->make_data_reader_tuple(
+        participant.make_data_reader_tuple(
             market_data_snapshot_full_refresh_topic_tuple,
             new MarketDataSnapshotFullRefreshDataReaderListener(app));
 
     // Execution Report response.
-    auto execution_report_topic_tuple = participant_ptr->make_topic<
+    auto execution_report_topic_tuple = participant.make_topic<
         DistributedStockExchange_ExecutionReport::ExecutionReportPubSubType,
         DistributedStockExchange_ExecutionReport::ExecutionReport>(
         EXECUTION_REPORT_TOPIC_NAME);
     auto execution_report_data_reader_tuple =
-        participant_ptr->make_data_reader_tuple(
+        participant.make_data_reader_tuple(
             execution_report_topic_tuple,
             new ExecutionReportDataReaderListener(app), "FILTERED_EXEC_REPORT",
             target_comp_id_filter, {fix_gateway_name});
 
     // Order Cancel Reject response.
-    auto order_cancel_reject_topic_tuple = participant_ptr->make_topic<
+    auto order_cancel_reject_topic_tuple = participant.make_topic<
         DistributedStockExchange_OrderCancelReject::OrderCancelRejectPubSubType,
         DistributedStockExchange_OrderCancelReject::OrderCancelReject>(
         ORDER_CANCEL_REJECT_TOPIC_NAME);
     auto order_cancel_reject_data_reader_tuple =
-        participant_ptr->make_data_reader_tuple(
+        participant.make_data_reader_tuple(
             order_cancel_reject_topic_tuple,
             new OrderCancelRejectDataReaderListener(app),
             "FILTERED_ORDER_CANCEL_REJECT", target_comp_id_filter,
             {fix_gateway_name});
 
-    // Order Mass Cancel Report reponse.
-    auto order_mass_cancel_report_topic_tuple = participant_ptr->make_topic<
+    // Order Mass Cancel Report response.
+    auto order_mass_cancel_report_topic_tuple = participant.make_topic<
         DistributedStockExchange_OrderMassCancelReport::
             OrderMassCancelReportPubSubType,
         DistributedStockExchange_OrderMassCancelReport::OrderMassCancelReport>(
         ORDER_MASS_CANCEL_REPORT_TOPIC_NAME);
     auto order_mass_cancel_report_data_reader_tuple =
-        participant_ptr->make_data_reader_tuple(
+        participant.make_data_reader_tuple(
             order_mass_cancel_report_topic_tuple,
             new OrderMassCancelReportDataReaderListener(app),
             "FILTERED_ORDER_MASS_CANCEL", target_comp_id_filter,
@@ -292,8 +280,8 @@ int main(int argc, char *argv[]) {
 
     // Make the socket acceptor and session factory to accept new FIX
     // connections, and manage current FIX sessions.
-    FixSocketAcceptorPtr acceptor_ptr = std::make_unique<FixSocketAcceptor>(
-        app, store_factory, *settings_ptr, log_factory);
+    FIX::SocketAcceptor acceptor(app, store_factory, *settings_ptr,
+                                 log_factory);
     FixSessionFactoryPtr session_factory_ptr =
         std::make_unique<FIX::SessionFactory>(app, store_factory, &log_factory);
 
@@ -304,7 +292,7 @@ int main(int argc, char *argv[]) {
     app.set_auth_service_ptr(std::move(auth_service_ptr));
 
     // Start accepting FIX socket connections.
-    acceptor_ptr->start();
+    acceptor.start();
 
     // Officially start the FIX gateway by flipping the flag on.
     std::atomic_init(&is_running, true);
@@ -331,7 +319,7 @@ int main(int argc, char *argv[]) {
 
     io_service.run();
 
-    acceptor_ptr->stop();
+    acceptor.stop();
   } catch (std::exception &e) {
     LOG4CXX_ERROR(logger, "Exception during the initialization of FIX Gateway: "
                               << e.what());
